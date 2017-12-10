0c303fd0301c1a7ecd0cd9a5efc0863b76ae494d
Nick Hoppe <hoppe@Nicks-MacBook-Air.local>
2017-12-10 09:34:21 -0500

    Updating

---
+++ fizzybuzzy.py
@@ -0,0 +1,18 @@
+
+#practicing a fizzbuzz
+#if divisible by 3 = fizzy, 5= buzzy, both =fizzybuzzy
+
+
+for x in range(1,50):
+    if x % 15==0:
+        print(x,"fizzybuzzy")
+    elif x % 5==0:
+        print(x,"buzzy")
+    else:
+        print(x,"fizzy")
+        
+        
+            
+        
+        
+
