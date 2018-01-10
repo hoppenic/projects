5282035f27b0b4086f06f1d75ff62e5502a4566d
Nick Hoppe <hoppenic@gmail.com>
2018-01-10 09:52:50 -0600

    update

--- playvideo.py
+++ playvideo.py
@@ -1,5 +1,10 @@
 
-#opens youtubve and plays a video
+#opens youtube and plays a video
+#runs after a certain amount of time
 
+import webbrowser
+import time
+
+time.sleep(10)
 webbrowser.open('https://www.youtube.com/watch?v=Jta56wBl7SM')
 
