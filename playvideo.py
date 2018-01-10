9845f361cb689c41849f5a324e6dadd56385aa4a
Nick Hoppe <hoppenic@gmail.com>
2018-01-10 10:21:31 -0600

    update

--- playvideo.py
+++ playvideo.py
@@ -1,6 +1,7 @@
 
 #opens youtube and plays a video
 #runs after a certain amount of time
+#I choose how long it waits between each running of the video
 
 import webbrowser
 import time
@@ -8,10 +9,11 @@ import time
 totalBreaks=3
 breakCount=0
 
+#loop here and run this as many times as I indicated in totalBreaks
+print('This program began on '+time.ctime())
+while(breakCount<totalBreaks):
+    time.sleep(5)   #the number waiting between runs in seconds
+    webbrowser.open('https://www.youtube.com/watch?v=Jta56wBl7SM')
+    breakCount+=1
 
-print('This program began on'+time.ctime())
-while(breakcount<totalBreaks):
-time.sleep(5)
-webbrowser.open('https://www.youtube.com/watch?v=Jta56wBl7SM')
-breakCount+=1
-
+    
