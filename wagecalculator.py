8939be495fe9102c900161794676ed2ed1b3cff8
Nick Hoppe <hoppenic@gmail.com>
2018-01-11 11:14:07 -0600

    update

--- wagecalculator.py
+++ wagecalculator.py
@@ -4,8 +4,8 @@
 #ask for pay per hour
 
 
-hoursWorked=input('How many hours did you work?')
-payHour=input('How much do you make per hour?')
+hoursWorked=input('How many hours did you work? ')
+payHour=input('How much do you make per hour? ')
 
 #float(hoursWorked)=hoursWorked
 #float(payHour)=payHour
@@ -17,13 +17,17 @@ if float(hoursWorked) <= 40:
 else:
     totalPay=(float(hoursWorked)-40)*(float(payHour)*1.5) + (float(hoursWorked)*float(payHour))
     
-#take out 401k(traditional)
-#take out taxes
+#take out 401k(traditional pre-tax)
 
-retirement=input('What percentage of your weekly salary should put put into your 401k?')
+retirement=input('What percentage of your weekly salary should put put into your 401k? ')
 retirement=(100-float(retirement))/100
 totalPay=totalPay*(float(retirement))
-    
+
+#take out taxes after 401k contribution
+
+tax=input('What marginal tax bracket are you in? ')
+tax=(100-float(tax))/100
+totalPay=totalPay*(float(tax))
     
 
 print('Your total weekly wage less retirement is $',totalPay)
