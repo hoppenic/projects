3b4485e6725a4f4d815fa9217437e6aa2e8925e7
Nick Hoppe <hoppenic@gmail.com>
2018-01-25 11:22:48 -0600

    update

---
+++ numpysurveys.py
@@ -0,0 +1,27 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+survey_responses = ['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos']
+
+print(len(survey_responses))
+
+#total voters for ceballos
+total_ceballos=sum([1 for n in survey_responses if n=='Ceballos'])
+print(total_ceballos)
+
+
+#percentage of voters for ceballos
+percentage_ceballos=100*total_ceballos/len(survey_responses)
+print(percentage_ceballos)
+
+
+possible_surveys=np.random.binomial(len(survey_responses),(percentage_ceballos/100),size=10000)
+possible_surveys=(possible_surveys/len(survey_responses))
+plt.hist(possible_surveys,bins=20, range=(0,1))
+plt.show()
+
+
